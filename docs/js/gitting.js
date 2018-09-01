aca0d08b369d93abc9a98f2f1f170d955391973a
Harvey Zack <laozhaochaguan@gmail.com>
2018-09-01 11:32:43 +0800

    uodate

--- docs/js/gitting.js
+++ docs/js/gitting.js
@@ -1,67 +1,67 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (global.gitting = factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.gitting = factory());
 }(this, (function () { 'use strict';
 
-    /*!
-     * isobject <https://github.com/jonschlinkert/isobject>
-     *
-     * Copyright (c) 2014-2017, Jon Schlinkert.
-     * Released under the MIT License.
-     */
+  /*!
+   * isobject <https://github.com/jonschlinkert/isobject>
+   *
+   * Copyright (c) 2014-2017, Jon Schlinkert.
+   * Released under the MIT License.
+   */
 
-    // 请求
+  // 请求
 
-    var classCallCheck = function (instance, Constructor) {
-      if (!(instance instanceof Constructor)) {
-        throw new TypeError("Cannot call a class as a function");
+  var classCallCheck = function (instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  };
+
+  var createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
       }
+    }
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
     };
+  }();
 
-    var createClass = function () {
-      function defineProperties(target, props) {
-        for (var i = 0; i < props.length; i++) {
-          var descriptor = props[i];
-          descriptor.enumerable = descriptor.enumerable || false;
-          descriptor.configurable = true;
-          if ("value" in descriptor) descriptor.writable = true;
-          Object.defineProperty(target, descriptor.key, descriptor);
-        }
+  var Gitting = function () {
+    function Gitting(option) {
+      classCallCheck(this, Gitting);
+
+      this.option = Object.assign({}, Gitting.DEFAULTS, option);
+      console.log("development");
+    }
+
+    createClass(Gitting, [{
+      key: "render",
+      value: function render(el) {
+        this.container = el instanceof Element ? el : document.querySelector(el);
       }
-
-      return function (Constructor, protoProps, staticProps) {
-        if (protoProps) defineProperties(Constructor.prototype, protoProps);
-        if (staticProps) defineProperties(Constructor, staticProps);
-        return Constructor;
-      };
-    }();
-
-    var Gitting = function () {
-      function Gitting(option) {
-        classCallCheck(this, Gitting);
-
-        this.option = Object.assign({}, Gitting.DEFAULTS, option);
-        console.log("development");
+    }], [{
+      key: "DEFAULTS",
+      get: function get$$1() {
+        return {};
       }
-
-      createClass(Gitting, [{
-        key: "render",
-        value: function render(el) {
-          this.container = el instanceof Element ? el : document.querySelector(el);
-        }
-      }], [{
-        key: "DEFAULTS",
-        get: function get$$1() {
-          return {};
-        }
-      }]);
-      return Gitting;
-    }();
-
-    window.Gitting = Gitting;
-
+    }]);
     return Gitting;
+  }();
+
+  window.Gitting = Gitting;
+
+  return Gitting;
 
 })));
 //# sourceMappingURL=gitting.js.map
